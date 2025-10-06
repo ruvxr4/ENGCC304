0d046c5103a43504a241aafbc4906fb9e117dbc7
jaylyn <apisitjf2549@gmail.com>
2025-10-06 17:22:12 +0700

    Add files via upload

---
+++ lab4/lab4.2.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+    char id[11];
+    int hrs ;
+    float r, s ;
+
+printf("input the employees ID(Max. 10 chars):");
+
+scanf("%s", id);
+
+printf("input the working hrs:");
+
+scanf("%d", &hrs);
+
+printf("salary amount/hr:") ;
+
+scanf("%f", &r) ;
+
+s = hrs * r ;
+
+printf("\nexpected output:\n");
+
+printf("employees ID = %s\n", id);
+
+printf("salary = U$ %.2f\n", s);
+
+return 0;
+}//end main
+
