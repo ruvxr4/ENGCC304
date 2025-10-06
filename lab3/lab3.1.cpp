89b7b3ecacaa7277d08f603888064238d0a0155d
jaylyn <apisitjf2549@gmail.com>
2025-10-06 14:48:39 +0700

    Add files via upload

---
+++ lab3/lab3.1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int h, b, A;
+
+    printf("enter height: \n");
+
+    scanf("%d",&h);
+
+    printf("enter base: \n");
+
+    scanf("%d",&b);
+
+    A = (h*b)/2 ;
+
+    printf("Area = %d",A);
+
+    return 0;
+
+
+}
